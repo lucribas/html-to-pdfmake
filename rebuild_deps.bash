c903283b6b80cab4aa6c47741af1499e1bb52384
Luciano da Silva Ribas <lucribas@users.>
2024-12-09 14:29:10 -0300

    up

--- rebuild_deps.bash
+++ rebuild_deps.bash
@@ -1,4 +1,35 @@
 
+
+# se alterar o pdfkit
 cd ../pdfkit
+
 npm run build
-cd -
+git add .
+git commit
+git push
+git log
+
+# e se alterar o pdfmake
+cd ../pdfmake
+rm -rf package-lock.json node_modules/
+npm install
+grep lucribas package-lock.json
+npm run build
+
+git add .
+git commit
+git push
+
+# e se alterar o html-to-pdkmake
+cd ../html-to-pdkmake
+
+rm -rf package-lock.json node_modules/
+npm install
+grep lucribas package-lock.json
+
+# teste
+heaptrack node example_img_stream3.js
+
+git add .
+git commit
+git push
